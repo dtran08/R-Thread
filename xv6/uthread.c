b7bf197e74b6baffc1c40130492f899f67fa5240
kadhirash <kadhirash@gmail.com>
2020-03-17 09:54:14 -0700

    Added updated uthread files

--- xv6/uthread.c
+++ xv6/uthread.c
@@ -9,38 +9,37 @@ mutex_p lock2;
 
 
 void firstThread(void) {
-
-  lock_acquire(lock1); //begin crit selec
+  lock_acquire(lock1); //begin crit selec, LOCK!
   printf(1, "Thread \'%s\' has started running\n",current_thread->name);
 
   int i;
   for (i = 0; i < 10; i++) {
     printf(1, "Thread \'%s\' at 0x%x\n", current_thread->name, (int) current_thread);
-    thread_yield(); //yield at crit selec
+    thread_yield(); //yield at crit selec, allow next thread to run
   }
   printf(1, "Thread \'%s\' has finished; exititing\n",current_thread->name);
   
   current_thread->state = FREE;//reassign state
-  lock_release(lock1);//end crit selec
+  lock_release(lock1);//end crit selec, UNLOCK!
   thread_schedule();
 }
 
 
 
 void secondThread(void) {
-  lock_acquire(lock2);//begin crit selec
+  lock_acquire(lock2);//begin crit selec, LOCK!
   printf(1, "Thread \'%s\' has started running\n",current_thread->name);
   
   int i;
   for (i = 0; i < 10; i++) {
     printf(1, "Thread \'%s\' at 0x%x\n", current_thread->name, (int) current_thread);
-    thread_yield();//yield at crit selec
+    thread_yield();//yield at crit selec, allow next thread to run
   }
   
   printf(1, "Thread \'%s\' has finished; exiting\n",current_thread->name);
   
   current_thread->state = FREE;//reassign state
-  lock_release(lock2);//end crit selec
+  lock_release(lock2);//end crit selec, UNLOCK!
   thread_schedule();
 }
 
@@ -55,6 +54,7 @@ int main(int argc, char *argv[]) {
   init_lock(lock1);//init lock since queue
   init_lock(lock2);
 
+  //because firstThread and secondThread lock, only one instance of each can be run concurrently
   thread_create("Thread1",firstThread);
   thread_create("Thread2",firstThread);
   thread_create("Thread3",secondThread);
--- xv6/uthread.h
+++ xv6/uthread.h
@@ -31,7 +31,7 @@ struct mutex {
   int filled;
 };
 
-//similart struct tpes for these mutex types
+//similar struct tpes for these mutex types
 typedef struct mutex mutex_t, *mutex_p;
 
 //array of all threads, under type thread_t
@@ -43,22 +43,27 @@ thread_p next_thread;
 
 extern void thread_switch(thread_p, thread_p);
 
+//cureate a new thread with a running extra function (func optional)
 boolean thread_create(char* name, void *func) {
   thread_p t;
   boolean free = false;
 
+  //t = a thread representing the array all_thread
+  //t < 10
+  //if the state of the thread is currently free, the bool free is true
+  //checks for any instances of a free thread in memory in the array
   for (t = all_thread; t < all_thread + MAX_THREAD; t++) {
     if(t->state == FREE) {
         free=true;
         break;
       }
   }
-  if(free==false) {
+  if(free==false) {//if there are no free threads, we terminate and return error
     printf(2,"Thread \'%s\'creation unsuccessful. MAX_THREAD limit reached \n",name);
     return false;    // no space for threads available, max_threads limit reached
   }
 
-  else {
+  else {//stack things
     t->sp = (int) (t->stack + STACK_SIZE);   // set sp to the top of the stack
     t->sp -= 4;                              // space for return address
     * (int *) (t->sp) = (int)func;           // push return address on stack
@@ -75,19 +80,22 @@ void thread_schedule(void) {//schedule based on round robin
   thread_p t;
   boolean found = false;
 
-  t = current_thread;
-  while(t < all_thread + MAX_THREAD) {       // first search if a runnable thread is present after current_thread
+  t = current_thread;//our thread var t is the current thread
+  //parse through the all_thread starting from current location of t
+  while(t < all_thread + MAX_THREAD) {
+    //if the state of the NEXT t is runnable and it's NOT the current thread
     if (t->state == RUNNABLE && t != current_thread) {
-      next_thread = t;
-      found = true;
-      break;
+      next_thread = t;//assign the next thread to t
+      found = true; //we've found the next thread
+      break;//we're done
     }
-    t++;
+    t++;//loop incremental var
   }
 
-  if(found==false) {                         // if not found, search from beginning to position of current_thread
-    t = all_thread;
-    while(t != current_thread) {
+  //if we went through the all_thread and didn't find a RUNNABLE thread
+  if(found==false) { 
+    t = all_thread;//start at beginning of all_thread
+    while(t != current_thread) {//do the same thing as before but from the start to finish
       if (t->state == RUNNABLE) {
         next_thread = t;
         found = true;
@@ -97,15 +105,21 @@ void thread_schedule(void) {//schedule based on round robin
     }
   }
 
-  if(current_thread->state==RUNNABLE) {       // now if found is false, that means t = current_thread is only runnable thread
+  //given found stays false, that means the current thread is t and is the only RUNNABLE thread
+  if(current_thread->state==RUNNABLE) { 
     next_thread = t;
   }
 
-  if (next_thread == 0) {                     // as assigned previously to 0 during switches
+  //case if there are no more runnable threads
+  if (next_thread == 0) {
     printf(2, "thread_schedule: no more runnable threads;\n");
     exit();
   }
 
+  //if the current thread is not the next thread
+  //we move forward and assign the next state to RUNNING
+  //switch threads
+  //if the current thread IS the next thread, we're done
   if (current_thread != next_thread) {        // switch threads in this case
     next_thread->state = RUNNING;
     thread_switch(current_thread, next_thread);
@@ -115,12 +129,20 @@ void thread_schedule(void) {//schedule based on round robin
   }
 }
 
+//sets the current thread's state to RUNNABLE and run through the scheduler
+//usually a wait function
 void thread_yield(void) {
   current_thread->state = RUNNABLE;
   thread_schedule();
 }
 
+
 //lock/mutex things
+
+//a busy-waiting model; the thread that's trying to acquire the lock WILL fail, and yield until it's ready
+//while the lock is acquired (true), keep yielding
+//once the lock is not acquired (false), assign it to true
+//this function waits for the lock to be released before relocking it for the next thread
 void lock_busy_wait_acquire(mutex_p lock) {//yield the thread if the lock is acquired
   while(lock->acq == true) {
     thread_yield();
@@ -128,41 +150,62 @@ void lock_busy_wait_acquire(mutex_p lock) {//yield the thread if the lock is acq
   lock->acq = true;
 }
 
+//releases the lock (acquired if false), putting it up for locking
 void lock_busy_wait_release(mutex_p lock) {
   lock->acq = false;
 }
 
-
-void lock_acquire(mutex_p lock) {//acquire lock
+//acquire/lock the lock (true) if the lock isn't already locked (acquired)
+void lock_acquire(mutex_p lock) {
   if(lock->acq == false) {
     lock->acq = true;
   }
-  else {
+  else {//if the lock is already acquired, we need to wait on whatever's running it
+    //the current thread's state is WAITING to be released
     current_thread->state = WAITING;
+    //the queue (originally of size MAX_THREAD)
+    //queue AT back+1 % MAX_THREAD, aka back+1, is assigned to current_thread
     lock->queue[(lock->back + 1) % MAX_THREAD] = current_thread;
-    lock->back = (lock->back + 1) % MAX_THREAD;     
+    //reassign lock->back to its value in the queue for current_thread
+    lock->back = (lock->back + 1) % MAX_THREAD;    
+    //increment filled to say that there's a lock that's filled 
     lock->filled++;
+    //schedule the next thread
     thread_schedule();
   }
 }
 
-void lock_release(mutex_p lock) {//release lock
+//release our lock, undoing lock_acquire; ONLY works if lock_acquire worked
+void lock_release(mutex_p lock) {
+  //release our lock!
   lock->acq = false;
+  //if our lock IS FILLED, aka not 0
   if(lock->filled != 0) {
+    //the lock at the previous state is now RUNNABLE
     lock->queue[lock->front]->state = RUNNABLE;
+    //reassign our lock's front to its value % MAX_THREAD, or just the front var
     lock->front = (lock->front+1)%MAX_THREAD;
+    //decrement the filled var, usually it's 1 so it shouldn't be a problem
     lock->filled--;
   }
 }
 
+//initialize our thread
 void thread_init(void) {
+  //we start our current_thread at the original point of all_thread
   current_thread = &all_thread[0];
+  //it's RUNNING since it's the first one!
   current_thread->state = RUNNING;
 }
 
+//initialize our mutex
 void init_lock(mutex_p lock) {
   lock->acq=false;
+  //for our release; enables the thread state to be RUNNABLE and increments itself
   lock->front=0;
-  lock->back=0;
+  //for our acquisition; after our current_thread is WAITING, the first pthread element is the current_thread
+  //allow it to increment
+  lock->back=-1;
+  //numerical acquisition of lock
   lock->filled=0;
 }
